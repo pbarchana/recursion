13bb9ca5c186d080006da94daa204fc4329bb1dc
pbarchana <pb_archana@yahoo.com>
2013-11-06 11:05:35 -0800

    exit conditions added

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,18 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+
+// EXIT conditions  begin
+
+ // If undefined, a function, or an XML value is encountered during conversion 
+ //it is either omitted (when it is found in an object) or censored to null (when it is found in an array).
+ // undefined may need modifications for handling.
+
+	if (obj === undefined) {
+		return obj;
+	} else if( typeof obj === 'string') {
+ 	 	return "\"" + obj + "\"";
+ 	} else
+  		return String(obj);
+// EXIT conditions end
 };
