9a0c510ac406968329b264666fb9db132a167369
pbarchana <pb_archana@yahoo.com>
2013-11-06 12:18:22 -0800

    Stringify - object handling added

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,31 +5,48 @@
 var stringifyJSON = function (obj) {
   // your code goes here
 
-// when parameter obj is an ARRAY
+// when obj is an ARRAY
 	if (Array.isArray(obj)) {
 		var sOutput = "";
 
-		_.each(obj, function (value, index, obj){
+		_.each(obj, function (value, index, obj) {
+			// If undefined, a function, or an XML value is encountered during conversion 
+ 			//censored to null when it is found in an array.
 			if (value === undefined || typeof(value) === "function" || typeof(value) === "xml")
 				sOutput += stringifyJSON(null) + ",";
 			else
 				sOutput += stringifyJSON(value) + ","; // sOutput will have a "," after the last element
 		});
 		return "[" + sOutput.substr(0, sOutput.length - 1) + "]";
-	}
+	} 
 
+// when obj is OBJECT	
+	
+	if (typeof(obj) === "object" && obj != null) { //typeof(null) is "object"
+		var sOutput = "";
+
+		_.each(obj, function (value, key, obj) {
+			// If undefined, a function, or an XML value is encountered during conversion 
+ 			//it is omitted when it is found in an object.
+			if (value !== undefined || typeof(value) !== "function" || typeof(value) !== "xml") {
+				sOutput += "\"" + String(key) + "\":"  ; 
+				sOutput += stringifyJSON(value) + "," ;
+			}
+		});
+		if (sOutput.length > 0) {
+			// omitting the "," after the last key:value pair	
+			return "{" + sOutput.substr(0, sOutput.length - 1) + "}"; 
+		}
+		return "{}"; 
+		} 
 
 // EXIT conditions  begin
-
- // If undefined, a function, or an XML value is encountered during conversion 
- //it is either omitted (when it is found in an object) or censored to null (when it is found in an array).
- // undefined may need modifications for handling.
-
 	if (obj === undefined) {
 		return obj;
-	} else if( typeof obj === 'string') {
+	} 
+	if( typeof obj === 'string') {   // String needs to be appended with quotes
  	 	return "\"" + obj + "\"";
- 	} else
-  		return String(obj);
+ 	} 
+  	return String(obj);
 // EXIT conditions end
 };
